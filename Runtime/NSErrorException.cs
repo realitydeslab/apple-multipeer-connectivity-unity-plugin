225934c407dfc3206499f5afea670c5c905eb301
yuchen <yuchenz27@outlook.com>
2023-05-11 16:29:57 +0800

    update license

--- Runtime/NSErrorException.cs
+++ Runtime/NSErrorException.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2023 Holo Interactive <dev@holoi.com>
+// SPDX-FileCopyrightText: Copyright 2020 Unity Technologies ApS
 // SPDX-FileContributor: Yuchen Zhang <yuchen@holoi.com>
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT AND LicenseRef-Unity-Companion-License
 
 using System;
 
